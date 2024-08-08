f672eecd47b4bc365fdc2226e8164e3d9df9a4e2
maxminly <84544830@qq.com>
2024-08-08 17:10:10 +0800

    Update flomo_api.py
    过滤已删除的memo

--- flomo/flomo_api.py
+++ flomo/flomo_api.py
@@ -59,7 +59,10 @@ class FlomoApi:
             print("get_memo_list business error:" + response_json['message'])
             return
 
-        return response_json['data']
+        # 过滤已删除的条目
+        memo_list = response_json['data']
+        filtered_memo_list = [memo for memo in memo_list if not memo.get('deleted_at', None)]
+        return filtered_memo_list
 
     def get_login_wechat_qrcode(self):
         pass
